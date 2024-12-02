165fdacf35376dd4801d8270afe3a9fffc006529
Ameera <ameera.cc@maxwellos.com>
2024-12-02 12:47:38 +0300

    search restaurent updated

--- src/app/shared/hotel-card/hotel-card.component.ts
+++ src/app/shared/hotel-card/hotel-card.component.ts
@@ -16,7 +16,7 @@ export class HotelCardComponent implements OnInit {
 
 
   isModalOpen = false;
-  qrCodeImage: string = ''; // Update if QR codes are provided dynamically
+  qrCodeImage: string = '';
   selectedHotelName: string = '';
   selectedHotelDeal: string = '';
   searchTerm: string = '';
@@ -42,9 +42,9 @@ export class HotelCardComponent implements OnInit {
     );
   }
 
-  // Update filteredHotelCards dynamically when searchTerm changes
-  getFilteredHotelCards() {
-    return this.hotelCards.filter(
+  // Updates the filtered list whenever searchTerm changes
+  onSearchTermChange() {
+    this.filteredHotelCards = this.hotelCards.filter(
       (card) =>
         card.restaurant_name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
         card.description.toLowerCase().includes(this.searchTerm.toLowerCase())
@@ -52,9 +52,9 @@ export class HotelCardComponent implements OnInit {
   }
 
   openModal(card: any) {
-    this.qrCodeImage = card.image; // Assuming this is the image to show in QR modal
+    this.qrCodeImage = card.image;
     this.selectedHotelName = card.restaurant_name;
-    this.selectedHotelDeal = this.stripHtml(card.description); // Remove HTML tags
+    this.selectedHotelDeal = this.stripHtml(card.description);
     this.isModalOpen = true;
   }
 
@@ -65,7 +65,7 @@ export class HotelCardComponent implements OnInit {
     this.selectedHotelDeal = '';
   }
 
-  // Helper function to strip HTML from description
+  // Helper function to strip HTML tags
   private stripHtml(html: string): string {
     const div = document.createElement('div');
     div.innerHTML = html;
@@ -73,103 +73,6 @@ export class HotelCardComponent implements OnInit {
   }
 
 
-  
-
-  // isModalOpen = false;
-  // qrCodeImage: string = '';
-  // selectedHotelName: string = '';
-  // selectedHotelDeal: string = '';
-  // searchTerm: string = '';
-  // hotelCards: any[] = [];
-  // filteredHotelCards: any[] = [];
-
-  // constructor(private restaurantService: RestaurentService) {}
-
-  // ngOnInit() {
-  //   this.fetchRestaurants();
-  // }
-
-  // fetchRestaurants() {
-  //   this.restaurantService.getAllRestaurants().subscribe(
-  //     (response) => {
-  //       this.hotelCards = response.data; 
-  //       this.filteredHotelCards = this.hotelCards; 
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching restaurant data:', error);
-  //     }
-  //   );
-  // }
-
-  // getFilteredHotelCards() {
-  //   return this.hotelCards.filter((card) =>
-  //     card.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-  //     card.description.toLowerCase().includes(this.searchTerm.toLowerCase())
-  //   );
-  // }
-
-  // openModal(card: any) {
-  //   this.qrCodeImage = card.qrCode;
-  //   this.selectedHotelName = card.restaurant_name;
-  //   this.selectedHotelDeal = card.description;
-  //   this.isModalOpen = true;
-  // }
-
-  // closeModal() {
-  //   this.isModalOpen = false;
-  //   this.qrCodeImage = '';
-  //   this.selectedHotelName = '';
-  //   this.selectedHotelDeal = '';
-  // }
-
-
-  // isModalOpen = false;
-  // qrCodeImage: string = '';
-  // selectedHotelName: string = '';
-  // selectedHotelDeal: string = '';
-  // searchTerm: string = '';
-
-  // hotelCards = [
-  //   {
-  //     imgSrc: '/images/restaurent/hotel1.png',
-  //     title: 'Zaytoon',
-  //     description: '20% off on all menu items!',
-  //     qrCode: '/images/restaurent/QRCodes/QRCode_1.2.png'
-  //   },
-  //   {
-  //     imgSrc: '/images/restaurent/hotel 2.png',
-  //     title: 'Tea Time',
-  //     description: '15% off on desserts!',
-  //     qrCode: '/images/restaurent/QRCodes/QRCode_2.png'
-  //   },
-  //   {
-  //     imgSrc: '/images/restaurent/hotel 2.png',
-  //     title: 'Hot N Cool',
-  //     description: 'Buy 1 Get 1 on Meals',
-  //     qrCode: '/images/restaurent/QRCodes/QRCode_3.jpeg'
-  //   }
-  // ];
-
-  // get filteredHotelCards() {
-  //   return this.hotelCards.filter(card => 
-  //     card.title.toLowerCase().includes(this.searchTerm.toLowerCase()) || 
-  //     card.description.toLowerCase().includes(this.searchTerm.toLowerCase())
-  //   );
-  // }
-
-  // openModal(card: any) {
-  //   this.qrCodeImage = card.qrCode;
-  //   this.selectedHotelName = card.title;
-  //   this.selectedHotelDeal = card.description;
-  //   this.isModalOpen = true;
-  // }
-
-  // closeModal() {
-  //   this.isModalOpen = false;
-  //   this.qrCodeImage = '';
-  //   this.selectedHotelName = '';
-  //   this.selectedHotelDeal = '';
-  // }
 }
 
 
